cbca115074d028e311270868d198a8b808ae4f8f
Georgiy_Oriabinskiy <102317474+GorgiY1@users.noreply.github.com>
2024-10-11 16:05:16 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [TaskController::class, 'read'])
-->name('task.read')->middleware(['auth', 'verified']);
+->name('task.read');
 
 
 Route::get('/create', [TaskController::class, 'create'])
